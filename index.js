01f5ec5566d1c69b6970fc974b823a8bc2ea994e
missingsemicolononline8 <76899808+missingsemicolononline8@users.noreply.github.com>
2023-11-09 19:00:40 +0530

    Deploy

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { extract } from '@extractus/article-extractor';
+const app = express();
+const PORT = "5001";
+import cors from "cors";
+import fetch from 'cross-fetch'
+import { HttpsProxyAgent } from 'https-proxy-agent'
+import { setSanitizeHtmlOptions } from "@extractus/article-extractor";
+
+const proxy = 'http://abc:RaNdoMpasswORd_country-France@proxy.packetstream.io:31113'
+
+
+app.use(cors());
+
+setSanitizeHtmlOptions({
+  allowedTags: [
+      "p","br","div"
+  ]
+})
+
+app.get('/extract', async(req,res) => {
+  const article = await extract(req.query.url)
+  console.log(article.content)
+  res.json(article);
+} )
+
+app.listen(PORT,() => {
+    console.log(`server running on http://localhost:${PORT}`)
+})
